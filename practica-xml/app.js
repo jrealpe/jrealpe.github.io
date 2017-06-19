ecad0d1b72630af6bad3a55156b3182b282f0cfc
Julio Realpe <jrealpe94@gmail.com>
2017-06-19 14:47:15 -0500

    [ADD] XML request and parse

---
+++ practica-xml/app.js
@@ -0,0 +1,60 @@
+var http_request = false;
+
+function makeRequest(url) {
+
+
+    http_request = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/xml');
+            // Ver nota sobre esta linea al final
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+
+    if (!http_request) {
+        alert('Falla :( No es posible crear una instancia XMLHTTP');
+        return false;
+    }
+    http_request.onreadystatechange = alertContents;
+    http_request.open('GET', url, true);
+    http_request.send(null);
+
+}
+
+function alertContents() {
+    if (http_request.readyState == 4) {
+        if (http_request.status == 200) {
+            var xmldoc = http_request.responseXML;
+
+
+            var canciones_node = xmldoc.getElementsByTagName('cancion');
+            //console.log(canciones_node.childNodes;
+            for (i = 0; i < canciones_node.length; i++) {
+                let l_canciones = document.getElementById('lista-canciones');
+                let item = document.createElement('li');
+                item.appendChild(document.createTextNode(canciones_node[i].attributes.getNamedItem('titulo').nodeValue));
+                l_canciones.appendChild(item);
+            }
+
+        } else {
+            alert('Hubo problemas con la petición.');
+        }
+    }
+}
+
+window.onload = function() {
+    var link = document.getElementById('requerimiento');
+    link.onclick = function() {
+        makeRequest('datos.xml');
+    }
+}
