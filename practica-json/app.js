3e0204f08299d20cdf8146e73deb2aa2886f3178
Julio Realpe <jrealpe94@gmail.com>
2017-06-19 15:09:46 -0500

    [ADD] JSON request and parser

---
+++ practica-json/app.js
@@ -0,0 +1,55 @@
+var http_request = false;
+
+function makeRequest(url) {
+
+
+    http_request = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/json');
+            // Ver nota sobre esta linea al final
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+
+    if (!http_request) {
+        alert('Falla :( No es posible crear una instancia XMLHTTP');
+        return false;
+    }
+    http_request.onreadystatechange = alertContents;
+    http_request.open('GET', url, true);
+    http_request.send(null);
+
+}
+
+function alertContents() {
+    if (http_request.readyState == 4) {
+        if (http_request.status == 200) {
+            var response = JSON.parse(http_request.responseText);
+            for (res in response) {
+                let l_canciones = document.getElementById('lista-canciones');
+                let item = document.createElement('li');
+                item.appendChild(document.createTextNode(response[res].titulo));
+                l_canciones.appendChild(item);
+            }
+        } else {
+            alert('Hubo problemas con la petición.');
+        }
+    }
+}
+
+window.onload = function() {
+    var link = document.getElementById('requerimiento');
+    link.onclick = function() {
+        makeRequest('datos.json');
+    }
+}
